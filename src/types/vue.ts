d8896cd0fd31f9c0e4085d8b0c37a0ccef85b177
1379255913 <1379255913@qq.com>
2023-04-11 12:58:17 +0800

    feat: add useTimeoutFn

--- src/types/vue.ts
+++ src/types/vue.ts
@@ -1,1 +1,7 @@
+import type { Ref } from 'vue'
+
 export type Emit = (event: string, ...args: any[]) => void;
+
+export type MaybeRef<T> = T | Ref<T>;
+
+export type MaybeRefArray<T> = T | Ref<T> | T[] | Ref<T[]>;
